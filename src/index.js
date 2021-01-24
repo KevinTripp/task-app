9bad627f8712a1cb28a0ef4d042e020b116e307b
unknown <ktripp1@gmail.com>
2021-01-23 21:18:41 -0500

    add debug code-timeout setting increased

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.use(function(req, res, next){
+    res.setTimeout(120000, function(){
+        console.log('Request has timed out.');
+            res.send(408);
+        });
+
+    next();
+});
+
 app.listen(port, () =>{
     console.log('Server is up on port '  + port)
 })
