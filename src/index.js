7fa0baf6599428738263ebdeca895d9bf03e3f49
unknown <ktripp1@gmail.com>
2021-01-23 20:38:32 -0500

    removing debugging code

--- src/index.js
+++ src/index.js
@@ -6,16 +6,9 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT
 
-console.log(1)
-
 app.use(express.json())
-console.log(2)
-
 app.use(userRouter)
-console.log(3)
-
 app.use(taskRouter)
-console.log(4)
 
 app.listen(port, () =>{
     console.log('Server is up on port '  + port)
