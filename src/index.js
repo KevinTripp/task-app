137157bafc4b2b188007663a72a7834be65194f5
unknown <ktripp1@gmail.com>
2021-01-23 20:29:20 -0500

    debugging attempt for crashed server

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT
 
+console.log(1)
+
 app.use(express.json())
+console.log(2)
+
 app.use(userRouter)
+console.log(3)
+
 app.use(taskRouter)
+console.log(4)
 
 app.listen(port, () =>{
     console.log('Server is up on port '  + port)
